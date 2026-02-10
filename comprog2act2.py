463aafc173be4d584118a1b18b6d65dd646b837f
Auvrey Patindol <auvrey.patindol20@gmail.com>
2026-02-10 17:38:34 +0800

    Create comprog2act2.py

---
+++ comprog2act2.py
@@ -0,0 +1,18 @@
+yourName = input("Enter your name: ")
+yourAge = int(input("Enter your age: "
+))
+yourHeight = float(input("Enter your
+height in meters: "))
+yourWork = input("Are you working?(Y/N
+): ")
+
+ifWorking = yourWork == "y" or yourwork
+== "y"
+
+print(f"\nYour name is: {yourName}")
+print (type (your Name ))
+print(f"Your age is: {yourAge}")
+print(f"Your height is: {yourHeight}")
+print (type(yourHeight))
+print(f"You are working: {ifWorking}")
+print (type(ifWorking))
