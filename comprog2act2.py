a2e2a20a3676d86e7820b8eabd7b50cbd7230448
Auvrey Patindol <auvrey.patindol20@gmail.com>
2026-02-10 17:45:51 +0800

    Update comprog2act2.py

--- comprog2act2.py
+++ comprog2act2.py
@@ -10,8 +10,9 @@ ifWorking = yourWork == "y" or yourwork
 == "y"
 
 print(f"\nYour name is: {yourName}")
-print (type (your Name ))
+print (type (yourName))
 print(f"Your age is: {yourAge}")
+print(type(yourAge))
 print(f"Your height is: {yourHeight}")
 print (type(yourHeight))
 print(f"You are working: {ifWorking}")
