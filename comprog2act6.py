bf4d5871c38036c0ef3e2b4b9c6c107f14004dff
Auvrey Patindol <auvrey.patindol20@gmail.com>
2026-02-21 17:15:06 +0800

    Add function to find the largest of three numbers

---
+++ comprog2act6.py
@@ -0,0 +1,9 @@
+def myFunc():
+    numA = input("Enter your first number: ")
+    numB = input("Enter your second number: ")          
+    numC = input("Enter your third number: ")
+    largest = max(numA, numB, numC)
+    print(f"{largest} is the largest number")
+
+
+myFunc()
